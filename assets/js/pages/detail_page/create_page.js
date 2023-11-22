bea84bfd40ad75c88382fc4cc4ec743ca839edb7
thanhnc1997 <thanhnc1997@gmail.com>
2023-11-22 16:54:01 +0700

    udpate 16:55PM

--- assets/js/pages/detail_page/create_page.js
+++ assets/js/pages/detail_page/create_page.js
@@ -12,6 +12,9 @@ export async function render(params) {
 	let date_list = [],
 			date_session = 'morning',
 			current_day = '',
+			empty_period = false,
+			morning_p = [],
+			afternoon_p = [],
 			create_obj = {};
 	
 	const template = await create_element('div');
@@ -22,8 +25,8 @@ export async function render(params) {
 			date_list.push(item.day.date);
 			
 			create_obj[item.day.date] = {
-				morning: [],
-				afternoon: []
+				morning: {},
+				afternoon: {}
 			}
 		});
 		
@@ -33,13 +36,15 @@ export async function render(params) {
 		//
 		date_list = [...new Set(date_list)];
 		for (let [k, v] of Object.entries(create_obj)) {
-			params.map(item => {
+			params.map((item, i) => {
 				if (item.day.date == k && item.day.session == true) {
-					create_obj[k]['morning'].push(item);
+					create_obj[k]['morning'][item.timetable_id] = item;
+					morning_p.push(item.timetable_id);
 				}
 				
 				if (item.day.date == k && item.day.session == false) {
-					create_obj[k]['afternoon'].push(item);
+					create_obj[k]['afternoon'][item.timetable_id] = item;
+					afternoon_p.push(item.timetable_id);
 				}
 			});
 		}
@@ -53,33 +58,140 @@ export async function render(params) {
 					template.querySelector('#days .tag-item.active').classList.remove('active');
 				}
 				item.classList.add('active');
-				await load_period({day: day, date_session: date_session});
+				current_day = day;
+				await load_period({day: current_day, date_session: date_session});
+				
+				if (date_session == 'morning') {
+					template.querySelector('.modal .period').appendChild(await load_period_detail(create_obj[current_day][date_session][morning_p[0]]));
+				}
+				
+				if (date_session == 'afternoon') {
+					template.querySelector('.modal .period').appendChild(await load_period_detail(create_obj[current_day][date_session][afternoon_p[0]]));
+				}
 			});
 			
 			template.querySelector('#days').appendChild(item);
 			template.querySelector('#days .tag-item').classList.add('active');
 		});
-		
 		console.log(create_obj);
 	}
 	
 	async function load_period(params) {
 		template.querySelector('#period').innerHTML = '';
 		let {day, date_session} = params;
-		create_obj[day][date_session].map(item => {
+		for (let [k, v] of Object.entries(create_obj[day][date_session])) {
 			let span = create_element('span');
 			span.classList.add('tag-item', 'square');
-			span.innerHTML = item.timetable_id;
-			span.addEventListener('click', () => {
+			span.innerHTML = k;
+			span.addEventListener('click', async () => {
 				if (template.querySelector('#period .tag-item.active')) {
 					template.querySelector('#period .tag-item.active').classList.remove('active');
 				}
 				span.classList.add('active');
+				
+				template.querySelector('.modal-body .period').appendChild(await load_period_detail(v));
 			});
 
 			template.querySelector('#period').appendChild(span);
 			template.querySelector('#period .tag-item').classList.add('active');
+		}
+	}
+	
+	async function load_period_detail(params) {
+		template.querySelector('.modal-body .period').innerHTML = '';
+		if (!params) return false;
+		
+		let _class = params.class;
+		let {day, subject, component_id, empty_period} = params;
+		
+		let div = create_element('div');
+		div.innerHTML = `
+		<h4 style="padding: 16px; background: #EBFAFF;">
+			Ngày ${day.date}
+		</h4>
+		<div style="padding: 16px;">
+			<div class="grid grid-3 gap-14 mb-14">
+				<div>
+					<label class="label required">Tiết PPTC</label>
+					<input class="input" name="component_id" value="${component_id}" disabled>
+				</div>
+				<div>
+					<label class="label required">Lớp</label>
+					<select class="select" name="classes">
+						
+					</select>
+				</div>
+				<div>
+					<label class="label required">Môn</label>
+					<select class="select" name="subjects">
+						
+					</select>
+				</div>
+			</div>
+			<div class="mb-14">
+				<label class="label required">Tên bài dạy</label>
+				<input class="input" placeholder="Điền tên bài dạy">
+			</div>
+			<div class="mb-14">
+				<label class="label required">Chuẩn bị điều chỉnh (TN, hoặc thay tiết dạy)</label>
+				<input class="input" placeholder="Nội dung">
+			</div>
+			<div class="d-flex align-items-center">
+				<b class="mr-auto">Tiết trống</b>
+				<label class="switch">
+					<input type="checkbox" name="empty_period">
+					<span class="slider"></span>
+				</label>
+			</div>
+			<div id="note" style="margin-top: 14px; display: none;">
+				<input class="input" placeholder="Lý do trống tiết" name="note">
+			</div>
+		</div>
+		`;
+		
+		div.querySelector('input[name="empty_period"]').addEventListener('change', e => {
+			empty_period = !empty_period;
+			
+			if (empty_period == true) {
+				div.querySelector('#note').style.display = 'block';
+			}
+			
+			if (empty_period == false) {
+				div.querySelector('#note').style.display = 'none';
+			}
 		});
+		
+		await fetch_data({
+			method: 'GET',
+			url: API_URL + API_END_POINT.classes,
+			auth: user.access_token,
+			async callback(params) {
+				params.map(item => {
+					let option = create_element('option');
+					option.innerHTML = item.name;
+					if (item.name == _class.name) option.setAttribute('selected', 'selected');
+					
+					div.querySelector('select[name="classes"]').appendChild(option);
+				});
+			} 
+		});
+		
+		await fetch_data({
+			method: 'GET',
+			url: API_URL + API_END_POINT.subjects,
+			auth: user.access_token,
+			async callback(params) {
+				params.map(item => {
+					let option = create_element('option');
+					option.innerHTML = item.name;
+					if (item.name == subject.name) option.setAttribute('selected', 'selected');
+					
+					div.querySelector('select[name="subjects"]').appendChild(option);
+				});
+			} 
+		});
+		
+		return div;
 	}
 	
 	async function modal_header() {
@@ -124,46 +236,7 @@ export async function render(params) {
 				
 			</div>
 		</div>
-		<h4 style="padding: 16px; background: #EBFAFF;">
-			Ngày 16/11/2023
-		</h4>
-		<div style="padding: 16px;">
-			<div class="grid grid-3 gap-14 mb-14">
-				<div>
-					<label class="label required">Tiết PPTC</label>
-					<select class="select" name="units">
-						
-					</select>
-				</div>
-				<div>
-					<label class="label required">Lớp</label>
-					<select class="select" name="classes">
-						
-					</select>
-				</div>
-				<div>
-					<label class="label required">Môn</label>
-					<select class="select" name="subjects">
-						
-					</select>
-				</div>
-			</div>
-			<div class="mb-14">
-				<label class="label required">Tên bài dạy</label>
-				<input class="input" placeholder="Điền tên bài dạy">
-			</div>
-			<div class="mb-14">
-				<label class="label required">Chuẩn bị điều chỉnh (TN, hoặc thay tiết dạy)</label>
-				<input class="input" placeholder="Nội dung">
-			</div>
-			<div class="d-flex align-items-center">
-				<b class="mr-auto">Tiết trống</b>
-				<label class="switch">
-					<input type="checkbox">
-					<span class="slider"></span>
-				</label>
-			</div>
-		</div>
+		<div class="period"></div>
 		`;
 		
 		div.querySelectorAll('#session .tag-item').forEach(day => {
@@ -173,35 +246,15 @@ export async function render(params) {
 				date_session = e.currentTarget.getAttribute('data-session');
 				
 				load_period({day: current_day, date_session: date_session});
+				
+				if (date_session == 'morning') {
+					load_period_detail(create_obj[current_day][date_session][morning_p[0]]);
+				}
+				
+				if (date_session == 'afternoon') {
+					load_period_detail(create_obj[current_day][date_session][afternoon_p[0]]);
+				}
 			});
-		});
-		
-		await fetch_data({
-			method: 'GET',
-			url: API_URL + API_END_POINT.classes,
-			auth: user.access_token,
-			async callback(params) {
-				params.map(item => {
-					let option = create_element('option');
-					option.innerHTML = item.name;
-					
-					div.querySelector('select[name="classes"]').appendChild(option);
-				});
-			} 
-		});
-		
-		await fetch_data({
-			method: 'GET',
-			url: API_URL + API_END_POINT.subjects,
-			auth: user.access_token,
-			async callback(params) {
-				params.map(item => {
-					let option = create_element('option');
-					option.innerHTML = item.name;
-					
-					div.querySelector('select[name="subjects"]').appendChild(option);
-				});
-			} 
 		});
 		
 		return div;
@@ -235,6 +288,7 @@ export async function render(params) {
 			
 			await format_period_list(params);
 			await load_period({day: current_day, date_session:date_session});
+			await load_period_detail(create_obj[current_day][date_session][morning_p[0]]);
 		}
 	});
 	
